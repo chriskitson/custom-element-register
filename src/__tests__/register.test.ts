b711b5e5f3059a048d60bcb5370bab686a7cd3a8
Chris Kitson <rkitson@statestreet.com>
2019-01-28 11:35:23 +0000

    initial commit

---
+++ src/__tests__/register.test.ts
@@ -0,0 +1,60 @@
+import { Register } from '../register';
+
+interface ITestParams {
+  containerId: string,
+  name: string,
+  url: string
+}
+
+const params: ITestParams  = {
+  containerId: 'myTestContainer',
+  name: 'chris-app',
+  url: 'http://a-test.url/'
+};
+
+test('Register - container to be defined', () => {
+  const containerEl: HTMLElement = document.createElement('div')
+  containerEl.id = params.containerId;
+  document.body.append(containerEl);
+  expect(document.getElementById('myTestContainer')).toBeDefined();
+});
+
+test('Register - custom element tag created', () => {
+  const containerEl: HTMLElement = document.createElement('div')
+  containerEl.id = params.containerId;
+  document.body.append(containerEl);
+  Register(containerEl, params.name, params.url);
+  expect(containerEl.querySelectorAll('chris-app').length).toBeGreaterThan(0);
+});
+
+test('Register - custom script tag created', () => {
+  const containerEl: HTMLElement = document.createElement('div')
+  containerEl.id = params.containerId;
+  document.body.append(containerEl);
+  Register(containerEl, params.name, params.url);
+  expect(document.querySelectorAll('script').length).toBeGreaterThan(0);
+});
+
+test('Register - custom script tag id correct', () => {
+  const containerEl: HTMLElement = document.createElement('div')
+  containerEl.id = params.containerId;
+  document.body.append(containerEl);
+  Register(containerEl, params.name, params.url);
+  expect(document.querySelectorAll('script')[0].id).toEqual(params.name);
+});
+
+test('Register - custom script tag url correct', () => {
+  const containerEl: HTMLElement = document.createElement('div')
+  containerEl.id = params.containerId;
+  document.body.append(containerEl);
+  Register(containerEl, params.name, params.url);
+  expect(document.querySelectorAll('script')[0].src).toEqual(params.url);
+});
+
+// test('Register - custom element loaded', () => {
+//   const containerEl: HTMLElement = document.createElement('div')
+//   containerEl.id = params.containerId;
+//   document.body.append(containerEl);
+//   Register(containerEl, params.name, params.url);
+//   expect(document.querySelectorAll('script')[0].src).toEqual(params.url);
+// });
---
+++ src/register.ts
@@ -0,0 +1,17 @@
+export const Register = (container: HTMLElement, name: string, src: string): void => {
+  
+  // dynamically insert script (if doesn't already exist)
+  if (!document.getElementById(name)) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.id = name;
+    // script.onerror = () => console.error(`error loading ${src}`);
+    document.body.appendChild(script);
+  }
+  
+  // dynamically insert element
+  const element: HTMLElement = document.createElement(name);
+  // element.addEventListener('message', msg => console.log(msg));
+  container.appendChild(element);
+
+};
